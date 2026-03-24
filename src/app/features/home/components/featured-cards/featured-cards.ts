ce736c44042b22ece0c175be9ea5c1422c1be86c
cadelme <cdelgado.es@gmail.com>
2026-03-24 21:47:02 +0100

    add layout and styles featured-cards component

--- src/app/features/home/components/featured-cards/featured-cards.ts
+++ src/app/features/home/components/featured-cards/featured-cards.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-featured-cards',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './featured-cards.html',
   styles: ``,
 })
-export class FeaturedCards {}
+export class FeaturedCards {
+  public articles = Array.from({ length: 4 }, () => ({
+    title: 'Lorem ipsum',
+    description: 'Lorem ipsum dolor sit amet consectetur.',
+    link: '#',
+    date: 'Oct 24, 2025',
+  }));
+}
