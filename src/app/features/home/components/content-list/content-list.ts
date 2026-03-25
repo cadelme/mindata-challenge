a888ae0a51d92c92472f9444c5c8cedd7d928b89
cadelme <cdelgado.es@gmail.com>
2026-03-25 21:23:15 +0100

    adapt components home-banner & content-list to tablet layout

--- src/app/features/home/components/content-list/content-list.ts
+++ src/app/features/home/components/content-list/content-list.ts
@@ -6,11 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './content-list.html',
   styles: ``,
 })
-export class ContentList {
-  public articles = Array.from({ length: 4 }, () => ({
-    date: 'Lorem ipsum dolor sit amet.',
-    title: 'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et.',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.',
-  }));
-}
+export class ContentList {}
